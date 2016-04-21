e3e2e804d1cf4479e1652cbcd4fd0d538d4cf216
Daniel Dreier <d@puppetlabs.com>
2016-04-21 16:01:11 -0700

    bump to version 0.0.3

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 NAME=haproxy_check
-VERSION=0.0.2
+VERSION=0.0.3
 
 .PHONY: package
 package:
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Haproxy_check.Mixfile do
 
   def project do
     [app: :haproxy_check,
-     version: "0.0.2",
+     version: "0.0.3",
      language: :erlang,
      escript: escript,
      deps: deps]
